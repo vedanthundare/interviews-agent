33f507f681580618289fd903d78e11ba8221d941
Vedant <vedanthundare8@gmail.com>
2025-04-26 10:45:21 +0530

    updated code - resolverd the auth from and foremfield errors

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Controller, Control, FieldValues, Path } from 'react-hook-form';
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
+
 import {
   FormItem,
   FormLabel,
   FormControl,
-  FormMessage
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface FormFieldProps<T extends FieldValues> {
   control: Control<T>;
   name: Path<T>;
   label: string;
   placeholder?: string;
-  type?: 'text' | 'email' | 'password' | 'file';
+  type?: "text" | "email" | "password";
 }
 
 const FormField = <T extends FieldValues>({
@@ -21,21 +21,28 @@ const FormField = <T extends FieldValues>({
   name,
   label,
   placeholder,
-  type = 'text',
-}: FormFieldProps<T>) => (
-  <Controller
-    name={name}
-    control={control}
-    render={({ field }) => (
-      <FormItem>
-        <FormLabel>{label}</FormLabel>
-        <FormControl>
-          <Input className='input' placeholder={placeholder} type={type} {...field} />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    )}
-  />
-);
+  type = "text",
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input
+              className="input"
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
 
 export default FormField;
