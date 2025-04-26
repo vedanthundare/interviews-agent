994ded20e1549775c0849ba2f5d7f09f93378ab4
Vedant <vedanthundare8@gmail.com>
2025-04-26 11:43:11 +0530

    update main fi8le

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,53 +1,119 @@
-<<<<<<< HEAD
-import InterviewCard from '@/components/InterviewCard'
-import { Button } from '@/components/ui/button'
-import { dummyInterviews } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-=======
->>>>>>> fe80aff24bd8bb19c6b3a8b302794939ce2d8848
-import React from 'react'
+import dayjs from "dayjs";
+import Link from "next/link";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+import {
+  getFeedbackByInterviewId,
+  getInterviewById,
+} from "@/lib/actions/general.action";
+import { Button } from "@/components/ui/button";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const Feedback = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  const user = await getCurrentUser();
+
+  const interview = await getInterviewById(id);
+  if (!interview) redirect("/");
+
+  const feedback = await getFeedbackByInterviewId({
+    interviewId: id,
+    userId: user?.id!,
+  });
+
   return (
-<<<<<<< HEAD
-    <>
-    <section className='card-cta'>
-      <div className='flex flex-col gap-6 max-w-lg'>
-        <h2>Get Interview Ready with Ai-Powered Practice & Feedback</h2>
-        <p className='text-lg'> Practice on real Interview Questions and get Real Feedback</p>
-        <Button asChild className='btn-primary max-sm:w-full'>
-          <Link href="/interview">Start an Interview</Link>
+    <section className="section-feedback">
+      <div className="flex flex-row justify-center">
+        <h1 className="text-4xl font-semibold">
+          Feedback on the Interview -{" "}
+          <span className="capitalize">{interview.role}</span> Interview
+        </h1>
+      </div>
+
+      <div className="flex flex-row justify-center ">
+        <div className="flex flex-row gap-5">
+          {/* Overall Impression */}
+          <div className="flex flex-row gap-2 items-center">
+            <Image src="/star.svg" width={22} height={22} alt="star" />
+            <p>
+              Overall Impression:{" "}
+              <span className="text-primary-200 font-bold">
+                {feedback?.totalScore}
+              </span>
+              /100
+            </p>
+          </div>
+
+          {/* Date */}
+          <div className="flex flex-row gap-2">
+            <Image src="/calendar.svg" width={22} height={22} alt="calendar" />
+            <p>
+              {feedback?.createdAt
+                ? dayjs(feedback.createdAt).format("MMM D, YYYY h:mm A")
+                : "N/A"}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <p>{feedback?.finalAssessment}</p>
+
+      {/* Interview Breakdown */}
+      <div className="flex flex-col gap-4">
+        <h2>Breakdown of the Interview:</h2>
+        {feedback?.categoryScores?.map((category, index) => (
+          <div key={index}>
+            <p className="font-bold">
+              {index + 1}. {category.name} ({category.score}/100)
+            </p>
+            <p>{category.comment}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h3>Strengths</h3>
+        <ul>
+          {feedback?.strengths?.map((strength, index) => (
+            <li key={index}>{strength}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h3>Areas for Improvement</h3>
+        <ul>
+          {feedback?.areasForImprovement?.map((area, index) => (
+            <li key={index}>{area}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="buttons">
+        <Button className="btn-secondary flex-1">
+          <Link href="/" className="flex w-full justify-center">
+            <p className="text-sm font-semibold text-primary-200 text-center">
+              Back to dashboard
+            </p>
+          </Link>
+        </Button>
+
+        <Button className="btn-primary flex-1">
+          <Link
+            href={`/interview/${id}`}
+            className="flex w-full justify-center"
+          >
+            <p className="text-sm font-semibold text-black text-center">
+              Retake Interview
+            </p>
+          </Link>
         </Button>
       </div>
-      <Image src="/robot.png" alt='robo-dude' width={400} height={400} className='max-sm:hidden'/>
     </section>
+  );
+};
 
-    <section className='flex flex-col gap-6 mt-8'>
-      <h2>Your Interview's</h2>
-      <div className='interviews-section'>
-        {dummyInterviews.map((interview) =>(
-          <InterviewCard {...interview} key={interview.id}/>
-        ))}
-        <p>You havent taken ant interview yet</p>
-      </div>
-
-    </section>
-
-    <section className='flex flex-col gap-6 mt-8'>
-      <h2>Take an interview</h2>
-      <div className='interviews-section'>
-      {dummyInterviews.map((interview) =>(
-          <InterviewCard {...interview} />
-        ))}
-      </div>
-
-    </section>
-    </>
-=======
-    <div>Home Page</div>
->>>>>>> fe80aff24bd8bb19c6b3a8b302794939ce2d8848
-  )
-}
-
-export default Page
+export default Feedback;
